15b8a5c0f8dd3e7f8465edc6ff8ef8b87264e0c5
chuanhtoan <chuanhtoan139@gmail.com>
2021-03-02 14:42:43 +0700

    hamburger button show

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.querySelector(".header__info--fixed").classList.add('header__info--disable')
-        document.querySelector(".header__info--sticky").classList.remove('header__info--disable')
+        document.querySelector(".header__info--fixed").classList.add('disable')
+        document.querySelector(".header__info--sticky").classList.remove('disable')
+
+        document.querySelector(".header__info--hamburger-fixed").classList.add('disable')
+        document.querySelector(".header__info--hamburger-sticky").classList.remove('disable')
     } else {
-        document.querySelector(".header__info--sticky").classList.add('header__info--disable')
-        document.querySelector(".header__info--fixed").classList.remove('header__info--disable')
+        document.querySelector(".header__info--sticky").classList.add('disable')
+        document.querySelector(".header__info--fixed").classList.remove('disable')
+
+        document.querySelector(".header__info--hamburger-sticky").classList.add('disable')
+        document.querySelector(".header__info--hamburger-fixed").classList.remove('disable')
     }
 }
 
 function hamburger(x) {
-    x.classList.toggle("change");
+    x.classList.toggle("change")
+    if (x.classList.contains("change")) {
+        document.querySelector('#menu').style.opacity = '1';
+    } else {
+        document.querySelector('#menu').style.opacity = '0';
+    }
 }
 
-// function topFunction() {
-//     document.body.scrollTop = 0 // For Safari
-//     document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+// Media Query
+// var paragraph = document.querySelector('p');
+// var mediaQueryList = window.matchMedia('(max-width: 1140px)');
+// function screenTest(e) {
+//     if (e.matches) {
+//         document.querySelector("#fixed-btn-text").innerHTML = 'Đăng ký'
+//     } else {
+//         document.querySelector("#fixed-btn-text").innerHTML = 'ĐĂNG KÝ NHẬN VÉ MỜI'
+//     }
 // }
+// mediaQueryList.addListener(screenTest);
