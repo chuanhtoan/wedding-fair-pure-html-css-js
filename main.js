9c26102c7af225caa133925062919dc9b6ab6b7c
chuanhtoan <chuanhtoan139@gmail.com>
2021-03-02 11:27:14 +0700

    fixing detail

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ function scrollFunction() {
     }
 }
 
+function hamburger(x) {
+    x.classList.toggle("change");
+}
+
 // function topFunction() {
 //     document.body.scrollTop = 0 // For Safari
 //     document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
