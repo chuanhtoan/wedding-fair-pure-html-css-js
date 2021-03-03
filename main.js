253d6f3a82544993f5bb92cd8a1b47716ca1068f
chuanhtoan <chuanhtoan139@gmail.com>
2021-03-03 10:56:21 +0700

    cleaning code

--- main.js
+++ main.js
@@ -7,18 +7,12 @@ function scrollFunction() {
 
         document.querySelector(".header__info--hamburger-fixed").classList.add('disable')
         document.querySelector(".header__info--hamburger-sticky").classList.remove('disable')
-
-        // document.querySelector("#menu").style.top = '19%'
-        // document.querySelector("#overlay").style.top = '19%'
     } else {
         document.querySelector(".header__info--sticky").classList.add('disable')
         document.querySelector(".header__info--fixed").classList.remove('disable')
 
         document.querySelector(".header__info--hamburger-sticky").classList.add('disable')
         document.querySelector(".header__info--hamburger-fixed").classList.remove('disable')
-
-        // document.querySelector("#menu").style.top = '28%'
-        // document.querySelector("#overlay").style.top = '28%'
     }
 }
 
@@ -32,19 +26,4 @@ function hamburger(x) {
         document.querySelectorAll('.menu').forEach(menu => {menu.classList.remove('hidden')})
         document.querySelector("#overlay").classList.remove('hidden')
     }
-
-    // Show at position
-    // const ham = document.querySelector('.container')
 }
-
-// Media Query
-// var paragraph = document.querySelector('p');
-// var mediaQueryList = window.matchMedia('(max-width: 1140px)');
-// function screenTest(e) {
-//     if (e.matches) {
-//         document.querySelector("#fixed-btn-text").innerHTML = 'Đăng ký'
-//     } else {
-//         document.querySelector("#fixed-btn-text").innerHTML = 'ĐĂNG KÝ NHẬN VÉ MỜI'
-//     }
-// }
-// mediaQueryList.addListener(screenTest);
