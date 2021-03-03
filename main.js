042489c49e39e2e26e70ee590d4f636004f8086b
chuanhtoan <chuanhtoan139@gmail.com>
2021-03-03 10:17:21 +0700

    fixing image tags

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function scrollFunction() {
         document.querySelector(".header__info--hamburger-fixed").classList.add('disable')
         document.querySelector(".header__info--hamburger-sticky").classList.remove('disable')
 
-        document.querySelector("#menu").style.top = '19%'
-        document.querySelector("#overlay").style.top = '19%'
+        // document.querySelector("#menu").style.top = '19%'
+        // document.querySelector("#overlay").style.top = '19%'
     } else {
         document.querySelector(".header__info--sticky").classList.add('disable')
         document.querySelector(".header__info--fixed").classList.remove('disable')
@@ -17,21 +17,24 @@ function scrollFunction() {
         document.querySelector(".header__info--hamburger-sticky").classList.add('disable')
         document.querySelector(".header__info--hamburger-fixed").classList.remove('disable')
 
-        document.querySelector("#menu").style.top = '28%'
-        document.querySelector("#overlay").style.top = '28%'
+        // document.querySelector("#menu").style.top = '28%'
+        // document.querySelector("#overlay").style.top = '28%'
     }
 }
 
 function hamburger(x) {
     if (x.classList.contains("change")) {
         document.querySelectorAll('.container').forEach(hamburger => {hamburger.classList.remove("change")})
-        document.querySelector("#menu").classList.add('hidden')
+        document.querySelectorAll('.menu').forEach(menu => {menu.classList.add('hidden')})
         document.querySelector("#overlay").classList.add('hidden')
     } else {
         document.querySelectorAll('.container').forEach(hamburger => {hamburger.classList.add("change")})
-        document.querySelector("#menu").classList.remove('hidden')
+        document.querySelectorAll('.menu').forEach(menu => {menu.classList.remove('hidden')})
         document.querySelector("#overlay").classList.remove('hidden')
     }
+
+    // Show at position
+    // const ham = document.querySelector('.container')
 }
 
 // Media Query
